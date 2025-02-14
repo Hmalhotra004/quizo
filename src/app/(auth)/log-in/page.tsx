b4709b4db9cefacefafc6e72f22d86ad71bd03b1
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-02-14 19:50:13 +0530

    better login page

--- src/app/(auth)/log-in/page.tsx
+++ src/app/(auth)/log-in/page.tsx
@@ -9,17 +9,18 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import FormInput from "@/components/FormInput";
+import Label from "@/components/Label";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Loader2 } from "lucide-react";
 
 const formSchema = z.object({
-  name: z.string().min(1, "name is required"),
+  username: z.string().min(1, "Username is required"),
   password: z.string().min(6, "Password should be at least 6 characters"),
 });
 
@@ -31,7 +32,7 @@ const LoginPage = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      name: "",
+      username: "",
       password: "",
     },
   });
@@ -40,7 +41,7 @@ const LoginPage = () => {
     setIsLoading(true);
     try {
       const response = await axios.post("/api/login", {
-        username: values.name,
+        username: values.username,
         password: values.password,
       });
       if (response.status === 200) {
@@ -50,7 +51,7 @@ const LoginPage = () => {
     } catch (error) {
       if (axios.isAxiosError(error) && error.response?.status === 404) {
         const errorData = error.response.data;
-        form.setError("name", {
+        form.setError("username", {
           type: "manual",
           message: errorData,
         });
@@ -72,13 +73,13 @@ const LoginPage = () => {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center h-dvh">
-      <div className="mx-auto w-full sm:max-w-md border-stone-200 border-2">
-        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-stone-900">
-          Log In
+    <section className="flex flex-col justify-center items-center h-dvh">
+      <div className="mx-auto w-full sm:max-w-md">
+        <h2 className="text-center lg:text-3xl max-lg:text-2xl font-bold tracking-tight dark:text-white">
+          Log in to your account
         </h2>
         <div className="mt-2">
-          <div className="px-4 py-8 sm:px-10">
+          <div className="px-4 py-8 max-sm:px-10">
             <Form {...form}>
               <form
                 onSubmit={form.handleSubmit(onSubmit)}
@@ -86,10 +87,13 @@ const LoginPage = () => {
               >
                 <FormField
                   control={form.control}
-                  name="name"
+                  name="username"
                   render={({ field, fieldState }) => (
-                    <FormItem>
-                      <FormLabel id="name">Name</FormLabel>
+                    <FormItem className="relative">
+                      <Label
+                        fieldState={fieldState}
+                        label="Username"
+                      />
                       <FormControl>
                         <FormInput
                           id="name"
@@ -108,8 +112,11 @@ const LoginPage = () => {
                   control={form.control}
                   name="password"
                   render={({ field, fieldState }) => (
-                    <FormItem>
-                      <FormLabel id="password">Password</FormLabel>
+                    <FormItem className="relative">
+                      <Label
+                        fieldState={fieldState}
+                        label="Password"
+                      />
                       <FormControl>
                         <FormInput
                           id="password"
@@ -133,23 +140,27 @@ const LoginPage = () => {
                   type="submit"
                   className="w-full"
                 >
-                  Login
+                  {isLoading ? (
+                    <Loader2 className="animate-spin" />
+                  ) : (
+                    <span>Log in</span>
+                  )}
                 </Button>
               </form>
             </Form>
-            <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+            <div className="flex gap-2 justify-center xs:text-sm lg:text-base mt-6 px-2 text-gray-500">
               <div>Don&apos;t have an account</div>
               <a
                 href="/sign-up"
                 className="underline cursor-pointer"
               >
-                SignUp
+                Signup
               </a>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
